26f340581ed68728f2568c3077ff3c3a425b3464
David-Luis-Mora <davidluismora@gmail.com>
2025-01-28 20:27:43 +0000

    empezamos la serializacion

--- gameside/categories/views.py
+++ gameside/categories/views.py
@@ -1,8 +1,14 @@
 # Create your views here.
 
+from .models import Category
+from .serializers.categories_serializers import CategoriesSerializer
+
 
 def category_list(request):
-    pass
+    category_all = Category.objects.all()
+    serializer = CategoriesSerializer(category_all)
+
+    return serializer.serialize_instance()
 
 
 def category_detail(request):
